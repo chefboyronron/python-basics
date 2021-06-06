45356e6856ab05572d754a2c0636a87c11516246
chefboyronron <rgpseminiano@gmail.com>
2021-06-06 19:42:34 +0800

    chore: weight converter optimization

--- weight_converter.py
+++ weight_converter.py
@@ -1,50 +1,55 @@
 # Exercise - create weight converter from kg to lbs or lbs to kg
 
-def check_weight(weight):
-    if weight.isnumeric():
-        unit = input("(L)lb or (K)kg: ")
-        unit = unit.lower()
-        check_unit(unit, weight)
-    else:
-        weight = input("Please enter your weight: ")
-        check_weight(weight)
+# import regex module
+import re
 
 
 def check_unit(unit, weight):
+    # check if user input is [lLkK] else re enter the unit
     if unit != 'l' and unit != 'k':
-        unit = input('Unit only accepts (L)lb or (K)kg: ')
-        unit = unit.lower()
+        unit = input('Unit only accepts (L)lb or (K)kg: ').lower()
         check_unit(unit, weight)
     else:
+        # compute the weight based on the user input
         if unit == 'l':
-            mass = round(int(weight) * 0.453592, 2)
+            mass = round(weight * 0.45359237, 1)
             print(f'You are {mass} kg.')
         else:
-            mass = round(int(weight) * 2.20462, 2)
+            mass = round(weight * 2.205, 1)
             print(f'Your are {mass} lbs.')
 
-        is_finish = True
-        start_conversion(is_finish)
+        # after printing computation ask if the user want to convert again.
+        start_conversion(True)
 
 
-def start_conversion(is_finish):
-    if not is_finish:
+def start_conversion(finish):
+    # Check if the user want to end the conversion | default false
+    if not finish:
         weight = input("Please enter your weight: ")
-        check_weight(weight)
+        # check if the user input for weight is float or int else re enter the weight
+        if re.match('^[0-9.]*$', weight):
+            weight = float(weight)
+            unit = input("(L)lb or (K)kg: ").lower()
+            check_unit(unit, weight)
+        else:
+            start_conversion(False)
     else:
         repeat = input("Do you want to start again? Y or N ")
         repeat = repeat.upper()
 
+        # if the user not selected either [yYnN] end the conversion
         if repeat != 'Y' and repeat != 'N':
             print('Wrong selected value, ending conversion..')
             return True
 
+        # if the user want to convert again
         if repeat == 'Y':
-            is_finish = False
-            start_conversion(is_finish)
+            start_conversion(False)
         else:
+            # end the conversion
             return True
 
 
 is_finish = False
+# Execute conversion
 start_conversion(is_finish)
